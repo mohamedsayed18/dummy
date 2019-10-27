d6b12cf479b83f46d7bad19b0e9d59f2e8c1ce4d
leonVrashitov <student.ufa.vr@gmail.com>
2019-10-27 22:53:44 +0300

    update

--- manipulator.py
+++ manipulator.py
@@ -5,6 +5,15 @@ import json
 HOST = '127.0.0.1'
 PORT = 65432
 
+c = (
+    "\033[0m",   # End of color
+    "\033[36m",  # Cyan
+    "\033[91m",  # Red
+    "\033[35m",  # Magenta
+)
+
+
+
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -20,8 +29,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     break
                 else:
                     data = json.loads(data.decode())
+                print(c[3]+'--------------------------------------------'+ c[0] )
                 for i in data:
                     if data[i]['status'] == 'DOWN':
-                        logging.warning(' Controller: '+i+' ' + data[i]['status'] + ' ' +data[i]['datetime'] )
+                        logging.warning(c[2]+' Controller: '+i+' ' + data[i]['status'] + ' ' +data[i]['datetime']+ c[0] )
                     else:
-                        logging.info(' Controller: '+i+' ' + data[i]['status'] + ' ' +data[i]['datetime'] )
+                        logging.info(c[1]+' Controller: '+i+' ' + data[i]['status'] + ' ' +data[i]['datetime'] +c[0] )
